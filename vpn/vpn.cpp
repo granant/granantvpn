616c4a39c780ff47dc1048ded958e7ecf8e44792
granant@gmail.com <granant@gmail.com@27946b8c-599c-0f85-63d4-87e8a20d15be>
2012-05-31 18:36:00 +0000

    1.0.1.7 changed linker options to require admin permissions

--- vpn/vpn.cpp
+++ vpn/vpn.cpp
@@ -81,8 +81,44 @@ BOOL CvpnApp::OnCloseMessage( LPMSG pMsg )
 
 CvpnApp theApp;
 
+BOOL CvpnApp::IsUserAdmin() 
+{
+	BOOL b;
+	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+	PSID AdministratorsGroup; 
+	b = AllocateAndInitializeSid(
+		&NtAuthority,
+		2,
+		SECURITY_BUILTIN_DOMAIN_RID,
+		DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&AdministratorsGroup); 
+	if(b) 
+	{
+		if (!CheckTokenMembership( NULL, AdministratorsGroup, &b)) 
+		{
+			 b = FALSE;
+		} 
+		FreeSid(AdministratorsGroup); 
+	}
+	return(b);
+}
+
+
+
+
 BOOL CvpnApp::InitInstance()
 {
+
+	//if (IsUserAdmin()) 
+	//{
+	//	MessageBox(NULL, "admin", "Диагностика", MB_OK);
+	//}
+	//else {
+	//	MessageBox(NULL, "not admin", "Диагностика", MB_OK);
+	//}
+
+	
 	DeleteFile(EXE_OLD_NAME);
 	ASSERT( m_uMsgCheckInst == NULL ); // Only once
 	m_uMsgCheckInst = ::RegisterWindowMessage( EXE_NAME );
